9490579424d695881ed5332f13ca5f7dfe0e816c
subaru25801245 <subaru25801245@gmail.com>
2023-10-14 14:55:33 +0900

    新規投稿ページ画面へのルートを設定した

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('post', PostController::class);
 
 Route::get('/', function () {
     return view('welcome');
