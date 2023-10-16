2d58757c5f0afdff8709c445d6288cd1e7ffc7af
subaru25801245 <subaru25801245@gmail.com>
2023-10-16 15:43:38 +0900

    コメント機能の実装

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -16,6 +16,10 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('post', PostController::class);
+Route::post('post/comment/store', [CommentController::class, 'store'])->name('comment.store');
 
 Route::get('/', function () {
     return view('welcome');
